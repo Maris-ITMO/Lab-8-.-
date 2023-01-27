9295835a137e9985953e677c4c67b1f3d4d62682
Maris Shapiro <19maris72@gmail.com>
2023-01-27 23:45:36 +0300

    Добавьте файлы проекта.

---
+++ Lab 8СамРабота. Работа с файловой системой/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+namespace Lab_8СамРабота._Работа_с_файловой_системой
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Каталог, место на диске:");
+            string path = Console.ReadLine();
+            PrintDirectoryContents(path);
+            Console.ReadLine();
+        }
+
+        static void PrintDirectoryContents(string path)
+        {
+            try
+            {
+                string[] subDirectories = Directory.GetDirectories(path);
+                string[] files = Directory.GetFiles(path);
+
+                Console.WriteLine("Каталог: " + path);
+                Console.WriteLine("Файлы:");
+                foreach (string file in files)
+                {
+                    Console.WriteLine(file);
+                }
+                Console.WriteLine("Подкаталоги:");
+                foreach (string subDirectory in subDirectories)
+                {
+                    Console.WriteLine(subDirectory);
+                    PrintDirectoryContents(subDirectory);
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+                Console.WriteLine("Не верный путь Каталогу'{0}'.", path);
+            }
+        }
+    }
+}
+
+            
---
+++ new file 10 numbers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Создать новый файл и запишите в него 10 случайных чисел
+        string filePath = "randomNumbers.txt";
+        using (StreamWriter writer = new StreamWriter(filePath))
+        {
+            Random random = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                int number = random.Next(1, 100);
+                writer.WriteLine(number);
+            }
+        }
+
+        // Считайте числа из файла и вычислить сумму
+        int sum = 0;
+        using (StreamReader reader = new StreamReader(filePath))
+        {
+            while (!reader.EndOfStream)
+            {
+                int number = int.Parse(reader.ReadLine());
+                sum += number;
+            }
+        }
+
+        // Выведите сумму на консоль
+        Console.WriteLine("Sum: " + sum);
+        Console.ReadLine();
+    }
+}
